f66b530e8e930764f1be85dd7420cecc6afca27b
Harrison Liddiard <omniaura5@gmail.com>
2024-06-01 11:52:01 -0400

    add translate route request validation

--- server.py
+++ server.py
@@ -9,9 +9,20 @@ def index():
 @app.route('/api/translate', methods=['POST'])
 def translate():
     data = request.get_json()
+
+    request_error = None
     if 'text' not in data:
-        return jsonify({'error': 'No text provided'}), 400
+        request_error = 'No text provided'
+    if 'target_lang' not in data:
+        request_error = 'No "target_lang" (target language) provided'
+    if 'correlation' not in data:
+        request_error = 'No "correlation" (closeness to target language) provided'
+    
+    if request_error:
+        return jsonify({'error': request_error}), 400
+    
     return jsonify({'text': data['text']})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
